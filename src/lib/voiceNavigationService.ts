78a34f85b19350fdb77a9e91a8246f6f16367801
Geniuz <ahmadwajid@ucla.edu>
2026-06-19 16:48:23 -0700

    fix

--- src/lib/voiceNavigationService.ts
+++ src/lib/voiceNavigationService.ts
@@ -1106,6 +1106,7 @@ interface SpeechRecognition extends EventTarget {
   stop(): void;
 }
 
+// eslint-disable-next-line no-var
 declare var SpeechRecognition: {
   prototype: SpeechRecognition;
   new(): SpeechRecognition;
